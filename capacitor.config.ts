f0d29cacbade0db5fd8b38b6fb07c6989f4737b2
dayannapulla3 <dayannapulla3@gmail.com>
2025-03-05 20:06:51 +0000

    commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.camara.app',
   appName: 'camarapp',
-  webDir: 'dist'
+  webDir: 'dist/myapp/browser'
 };
 
 export default config;
